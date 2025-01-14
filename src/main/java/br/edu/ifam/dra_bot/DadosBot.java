1692100006d266e1670e5b27751ffd05fa04adf0
Sannyer3232 <139788768+Sannyer3232@users.noreply.github.com>
2025-01-14 10:01:58 -0400

    Rastreando o vcs.xml

--- src/main/java/br/edu/ifam/dra_bot/DadosBot.java
+++ src/main/java/br/edu/ifam/dra_bot/DadosBot.java
@@ -4,7 +4,7 @@ public class DadosBot {
     //Dados para conectar ao bot criado no telegram
 
     //Definindo como uma constante o Token para conexão com o Bot Telegram
-    public static final String BOT_TOKEN = "7638498199:AAEDfd4pIJ3am0RH2O8f8Mlh6Txj3Nm-hRg";
+    public static final String BOT_TOKEN = "7638498199:-hRgAAEDfd4pIJ3am0RH2O8f8Mlh6Txj3Nm";
 
     //Definindo o User name Criado no BotFather do Telegram
     public static final String BOT_USER_NAME = "dra_1_bot";
